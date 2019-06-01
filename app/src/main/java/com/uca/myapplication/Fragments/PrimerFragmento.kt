1bc2bdbffad9e6641fd0c6602b9c8390a0553cfc
GuillermoMorales <00011417@uca.edu.sv>
2019-06-01 08:14:14 -0600

    Empezando Fragmento Dos

--- app/src/main/java/com/uca/myapplication/Fragments/PrimerFragmento.kt
+++ app/src/main/java/com/uca/myapplication/Fragments/PrimerFragmento.kt
@@ -5,10 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.uca.myapplication.R
+import kotlinx.android.synthetic.main.fragmento_uno.*
 
 class PrimerFragmento: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragmento_uno,container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
